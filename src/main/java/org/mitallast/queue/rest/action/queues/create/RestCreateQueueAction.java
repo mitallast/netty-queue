622dd68b8e804ab36fa616045c34df8a8530090d
Alexey Korchevskiy <mitallast@gmail.com>
2014-10-25 22:46:33 +0400

    fix rest create queue action

--- src/main/java/org/mitallast/queue/rest/action/queues/create/RestCreateQueueAction.java
+++ src/main/java/org/mitallast/queue/rest/action/queues/create/RestCreateQueueAction.java
@@ -9,11 +9,13 @@ import org.mitallast.queue.action.queues.create.CreateQueueResponse;
 import org.mitallast.queue.client.Client;
 import org.mitallast.queue.common.settings.ImmutableSettings;
 import org.mitallast.queue.common.settings.Settings;
+import org.mitallast.queue.queues.QueueAlreadyExistsException;
 import org.mitallast.queue.rest.BaseRestHandler;
 import org.mitallast.queue.rest.RestController;
 import org.mitallast.queue.rest.RestRequest;
 import org.mitallast.queue.rest.RestSession;
 import org.mitallast.queue.rest.response.StatusRestResponse;
+import org.mitallast.queue.rest.response.StringRestResponse;
 
 public class RestCreateQueueAction extends BaseRestHandler {
 
@@ -37,7 +39,11 @@ public class RestCreateQueueAction extends BaseRestHandler {
 
             @Override
             public void onFailure(Throwable e) {
-                session.sendResponse(e);
+                if (e instanceof QueueAlreadyExistsException) {
+                    session.sendResponse(new StringRestResponse(HttpResponseStatus.CONFLICT, "Queue already exists"));
+                } else {
+                    session.sendResponse(e);
+                }
             }
         });
     }

72dbdb891196744200032db0284d0000a0c886fe
Alexey Korchevskiy <mitallast@gmail.com>
2014-10-26 12:07:39 +0300

    remove unused constructor

--- src/main/java/org/mitallast/queue/action/queue/delete/DeleteResponse.java
+++ src/main/java/org/mitallast/queue/action/queue/delete/DeleteResponse.java
@@ -3,6 +3,4 @@ package org.mitallast.queue.action.queue.delete;
 import org.mitallast.queue.action.ActionResponse;
 
 public class DeleteResponse extends ActionResponse {
-    public DeleteResponse() {
-    }
 }

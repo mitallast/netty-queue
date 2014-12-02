d98d59c0a3d513c9c67577ce53a8e66bdb75efa0
Alexey Korchevskiy <mitallast@gmail.com>
2014-12-02 23:31:43 +0300

    more netty related settings to transport servers at http and stomp servers

--- src/main/java/org/mitallast/queue/rest/transport/HttpServer.java
+++ src/main/java/org/mitallast/queue/rest/transport/HttpServer.java
@@ -3,7 +3,9 @@ package org.mitallast.queue.rest.transport;
 import com.google.inject.Inject;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.FixedRecvByteBufAllocator;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.mitallast.queue.QueueException;
@@ -20,10 +22,13 @@ public class HttpServer extends AbstractLifecycleComponent {
     private boolean reuseAddress;
     private boolean tcpNoDelay;
 
+    private int sndBuf;
+    private int rcvBuf;
+    private int wbLow;
+    private int wbHigh;
+
     private RestController restController;
 
-    private EventLoopGroup bossGroup;
-    private Class<? extends ServerChannel> channelClass;
     private ServerBootstrap bootstrap;
     private Channel channel;
 
@@ -32,33 +37,47 @@ public class HttpServer extends AbstractLifecycleComponent {
         super(settings);
         this.host = settings.get("host", "127.0.0.1");
         this.port = settings.getAsInt("port", 8080);
-        this.backlog = settings.getAsInt("backlog", 65536);
+        this.backlog = settings.getAsInt("backlog", 1024);
         this.reuseAddress = settings.getAsBoolean("reuse_address", true);
         this.keepAlive = settings.getAsBoolean("keep_alive", true);
-        this.tcpNoDelay = settings.getAsBoolean("tcp_no_delay", false);
+        this.tcpNoDelay = settings.getAsBoolean("tcp_no_delay", true);
+        this.sndBuf = settings.getAsInt("snd_buf", 4096 * 10);
+        this.rcvBuf = settings.getAsInt("rcv_buf", 4096 * 10);
+        this.wbHigh = settings.getAsInt("write_buffer_high_water_mark", 64 * 1024);
+        this.wbLow = settings.getAsInt("write_buffer_low_water_mark", 2 * 1024);
         this.restController = restController;
     }
 
     @Override
     protected void doStart() throws QueueException {
-        bossGroup = new NioEventLoopGroup();
-        channelClass = NioServerSocketChannel.class;
         try {
             bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup)
-                .channel(channelClass)
-                .childHandler(new HttpServerInitializer(new HttpServerHandler(restController)))
-                .option(ChannelOption.SO_BACKLOG, backlog)
-                .option(ChannelOption.SO_REUSEADDR, reuseAddress)
-                .option(ChannelOption.SO_KEEPALIVE, keepAlive)
-                .option(ChannelOption.TCP_NODELAY, tcpNoDelay)
-                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                .option(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT)
-                .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                .childOption(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT);
+            bootstrap.group(new NioEventLoopGroup())
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new HttpServerInitializer(new HttpServerHandler(restController)))
+                    .option(ChannelOption.SO_BACKLOG, backlog)
+                    .option(ChannelOption.SO_REUSEADDR, reuseAddress)
+                    .option(ChannelOption.SO_KEEPALIVE, keepAlive)
+                    .option(ChannelOption.TCP_NODELAY, tcpNoDelay)
+                    .option(ChannelOption.SO_SNDBUF, sndBuf)
+                    .option(ChannelOption.SO_RCVBUF, rcvBuf)
+                    .option(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, wbHigh)
+                    .option(ChannelOption.WRITE_BUFFER_LOW_WATER_MARK, wbLow)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(rcvBuf))
+
+                    .childOption(ChannelOption.SO_REUSEADDR, reuseAddress)
+                    .childOption(ChannelOption.SO_KEEPALIVE, keepAlive)
+                    .childOption(ChannelOption.TCP_NODELAY, tcpNoDelay)
+                    .childOption(ChannelOption.SO_SNDBUF, sndBuf)
+                    .childOption(ChannelOption.SO_RCVBUF, rcvBuf)
+                    .childOption(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, wbHigh)
+                    .childOption(ChannelOption.WRITE_BUFFER_LOW_WATER_MARK, wbLow)
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(rcvBuf));
             channel = bootstrap.bind(host, port)
-                .sync()
-                .channel();
+                    .sync()
+                    .channel();
         } catch (InterruptedException e) {
             throw new QueueException(e);
         }
@@ -66,7 +85,7 @@ public class HttpServer extends AbstractLifecycleComponent {
 
     @Override
     protected void doStop() throws QueueException {
-        bossGroup.shutdownGracefully();
+        bootstrap.group().shutdownGracefully();
         try {
             channel.closeFuture().sync();
         } catch (InterruptedException e) {
@@ -74,7 +93,6 @@ public class HttpServer extends AbstractLifecycleComponent {
         }
         channel = null;
         bootstrap = null;
-        bossGroup = null;
     }
 
     @Override
--- src/main/java/org/mitallast/queue/stomp/transport/StompServer.java
+++ src/main/java/org/mitallast/queue/stomp/transport/StompServer.java
@@ -3,7 +3,9 @@ package org.mitallast.queue.stomp.transport;
 import com.google.inject.Inject;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
-import io.netty.channel.*;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.FixedRecvByteBufAllocator;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import org.mitallast.queue.QueueException;
@@ -20,8 +22,10 @@ public class StompServer extends AbstractLifecycleComponent {
     private boolean keepAlive;
     private boolean reuseAddress;
     private boolean tcpNoDelay;
-    private EventLoopGroup bossGroup;
-    private Class<? extends ServerChannel> channelClass;
+    private int sndBuf;
+    private int rcvBuf;
+    private int wbLow;
+    private int wbHigh;
     private ServerBootstrap bootstrap;
     private Channel channel;
 
@@ -29,34 +33,48 @@ public class StompServer extends AbstractLifecycleComponent {
     public StompServer(Settings settings, StompController stompController) {
         super(settings);
         this.host = settings.get("host", "127.0.0.1");
-        this.port = settings.getAsInt("port", 9080);
-        this.backlog = settings.getAsInt("backlog", 65536);
+        this.port = settings.getAsInt("port", 8080);
+        this.backlog = settings.getAsInt("backlog", 1024);
         this.reuseAddress = settings.getAsBoolean("reuse_address", true);
         this.keepAlive = settings.getAsBoolean("keep_alive", true);
-        this.tcpNoDelay = settings.getAsBoolean("tcp_no_delay", false);
+        this.tcpNoDelay = settings.getAsBoolean("tcp_no_delay", true);
+        this.sndBuf = settings.getAsInt("snd_buf", 4096 * 10);
+        this.rcvBuf = settings.getAsInt("rcv_buf", 4096 * 10);
+        this.wbHigh = settings.getAsInt("write_buffer_high_water_mark", 64 * 1024);
+        this.wbLow = settings.getAsInt("write_buffer_low_water_mark", 2 * 1024);
         this.stompController = stompController;
     }
 
     @Override
     protected void doStart() throws QueueException {
-        bossGroup = new NioEventLoopGroup();
-        channelClass = NioServerSocketChannel.class;
         try {
             bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup)
-                .channel(channelClass)
-                .childHandler(new StompServerInitializer(new StompServerHandler(stompController)))
-                .option(ChannelOption.SO_BACKLOG, backlog)
-                .option(ChannelOption.SO_REUSEADDR, reuseAddress)
-                .option(ChannelOption.SO_KEEPALIVE, keepAlive)
-                .option(ChannelOption.TCP_NODELAY, tcpNoDelay)
-                .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                .option(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT)
-                .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
-                .childOption(ChannelOption.RCVBUF_ALLOCATOR, AdaptiveRecvByteBufAllocator.DEFAULT);
+            bootstrap.group(new NioEventLoopGroup())
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new StompServerInitializer(new StompServerHandler(stompController)))
+                    .option(ChannelOption.SO_BACKLOG, backlog)
+                    .option(ChannelOption.SO_REUSEADDR, reuseAddress)
+                    .option(ChannelOption.SO_KEEPALIVE, keepAlive)
+                    .option(ChannelOption.TCP_NODELAY, tcpNoDelay)
+                    .option(ChannelOption.SO_SNDBUF, sndBuf)
+                    .option(ChannelOption.SO_RCVBUF, rcvBuf)
+                    .option(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, wbHigh)
+                    .option(ChannelOption.WRITE_BUFFER_LOW_WATER_MARK, wbLow)
+                    .option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(rcvBuf))
+
+                    .childOption(ChannelOption.SO_REUSEADDR, reuseAddress)
+                    .childOption(ChannelOption.SO_KEEPALIVE, keepAlive)
+                    .childOption(ChannelOption.TCP_NODELAY, tcpNoDelay)
+                    .childOption(ChannelOption.SO_SNDBUF, sndBuf)
+                    .childOption(ChannelOption.SO_RCVBUF, rcvBuf)
+                    .childOption(ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK, wbHigh)
+                    .childOption(ChannelOption.WRITE_BUFFER_LOW_WATER_MARK, wbLow)
+                    .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+                    .childOption(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(rcvBuf));
             channel = bootstrap.bind(host, port)
-                .sync()
-                .channel();
+                    .sync()
+                    .channel();
         } catch (InterruptedException e) {
             throw new QueueException(e);
         }
@@ -64,7 +82,7 @@ public class StompServer extends AbstractLifecycleComponent {
 
     @Override
     protected void doStop() throws QueueException {
-        bossGroup.shutdownGracefully();
+        bootstrap.group().shutdownGracefully();
         try {
             channel.closeFuture().sync();
         } catch (InterruptedException e) {
@@ -72,7 +90,6 @@ public class StompServer extends AbstractLifecycleComponent {
         }
         channel = null;
         bootstrap = null;
-        bossGroup = null;
     }
 
     @Override

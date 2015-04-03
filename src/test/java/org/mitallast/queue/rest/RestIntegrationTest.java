f8438b142bf5c959a6e952a5ba4022b5293f837e
Alexey Korchevskiy <mitallast@gmail.com>
2015-04-03 14:19:38 +0300

    optimize rest integration test

--- src/test/java/org/mitallast/queue/rest/RestIntegrationTest.java
+++ src/test/java/org/mitallast/queue/rest/RestIntegrationTest.java
@@ -2,6 +2,8 @@ package org.mitallast.queue.rest;
 
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.*;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.mitallast.queue.common.BaseQueueTest;
 import org.mitallast.queue.common.strings.Strings;
@@ -12,6 +14,29 @@ import java.util.List;
 import java.util.concurrent.Future;
 
 public class RestIntegrationTest extends BaseQueueTest {
+
+    private List<RestClient> clients;
+    private int client;
+
+    @Before
+    public void setUpClient() throws Exception {
+        client = 0;
+        clients = new ArrayList<>();
+        for (int i = 0; i <= concurrency(); i++) {
+            RestClient restClient = new RestClient(settings());
+            restClient.start();
+            clients.add(restClient);
+        }
+    }
+
+    @After
+    public void tearDownClient() throws Exception {
+        clients.forEach(RestClient::stop);
+    }
+
+    private synchronized RestClient restClient() {
+        return clients.get(client++);
+    }
 
     @Test
     public void test() throws Exception {
@@ -36,38 +61,34 @@ public class RestIntegrationTest extends BaseQueueTest {
     }
 
     private void warmUp() throws Exception {
-        send(max());
+        send(1000);
     }
 
     private void send(int max) throws Exception {
-        RestClient restClient = new RestClient(settings());
-        restClient.start();
-        try {
-            logger.info("send");
-            byte[] bytes = "{\"message\":\"hello world\"}".getBytes(Strings.UTF8);
-            List<Future<FullHttpResponse>> futures = new ArrayList<>(max);
-            for (int i = 0; i < max; i++) {
-                DefaultFullHttpRequest request = new DefaultFullHttpRequest(
-                    HttpVersion.HTTP_1_1,
-                    HttpMethod.PUT,
-                    "/" + queueName() + "/message",
-                    Unpooled.wrappedBuffer(bytes),
-                    false
-                );
-                request.headers().set(HttpHeaderNames.CONTENT_LENGTH, bytes.length);
-                futures.add(restClient.send(request));
-            }
-            restClient.flush();
-            logger.info("await");
-            for (Future<FullHttpResponse> future : futures) {
-                FullHttpResponse response = future.get();
-                assert response.status().code() >= 200 : response.status();
-                assert response.status().code() < 300 : response.status();
-                response.content().release();
-            }
-            logger.info("await done");
-        } finally {
-            restClient.stop();
+        RestClient restClient = restClient();
+
+        logger.info("send");
+        byte[] bytes = "{\"message\":\"hello world\"}".getBytes(Strings.UTF8);
+        List<Future<FullHttpResponse>> futures = new ArrayList<>(max);
+        for (int i = 0; i < max; i++) {
+            DefaultFullHttpRequest request = new DefaultFullHttpRequest(
+                HttpVersion.HTTP_1_1,
+                HttpMethod.PUT,
+                "/" + queueName() + "/message",
+                Unpooled.wrappedBuffer(bytes),
+                false
+            );
+            request.headers().set(HttpHeaderNames.CONTENT_LENGTH, bytes.length);
+            futures.add(restClient.send(request));
         }
+        restClient.flush();
+        logger.info("await");
+        for (Future<FullHttpResponse> future : futures) {
+            FullHttpResponse response = future.get();
+            assert response.status().code() >= 200 : response.status();
+            assert response.status().code() < 300 : response.status();
+            response.content().release();
+        }
+        logger.info("await done");
     }
 }

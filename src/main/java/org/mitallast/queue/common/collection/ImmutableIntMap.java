ec5813a78937c558432ca56839618d120027063b
Alexey Korchevskiy <mitallast@gmail.com>
2015-04-08 18:01:27 +0300

    immutable map add empty

--- src/main/java/org/mitallast/queue/common/collection/ImmutableIntMap.java
+++ src/main/java/org/mitallast/queue/common/collection/ImmutableIntMap.java
@@ -2,12 +2,49 @@ package org.mitallast.queue.common.collection;
 
 public interface ImmutableIntMap<V> {
 
+    public final static ImmutableIntMap EMPTY = new ImmutableIntMap<Object>() {
+        @Override
+        public int emptyKey() {
+            return 0;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(int key) {
+            return false;
+        }
+
+        @Override
+        public Object get(int key) {
+            return null;
+        }
+
+        @Override
+        public void forEach(IntMapConsumer<Object> consumer) {
+
+        }
+    };
+
     public static <V> ImmutableIntMapBuilder<V> builder() {
         return new ImmutableIntOpenMapBuilder<V>();
     }
 
     public static <V> ImmutableIntMapBuilder<V> builder(int emptyKey) {
         return new ImmutableIntOpenMapBuilder<V>(emptyKey);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <V> ImmutableIntMap<V> empty() {
+        return EMPTY;
     }
 
     int emptyKey();
--- src/main/java/org/mitallast/queue/common/collection/ImmutableLongMap.java
+++ src/main/java/org/mitallast/queue/common/collection/ImmutableLongMap.java
@@ -2,12 +2,49 @@ package org.mitallast.queue.common.collection;
 
 public interface ImmutableLongMap<V> {
 
+    public final static ImmutableLongMap EMPTY = new ImmutableLongMap<Object>() {
+        @Override
+        public long emptyKey() {
+            return 0;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(long key) {
+            return false;
+        }
+
+        @Override
+        public Object get(long key) {
+            return null;
+        }
+
+        @Override
+        public void forEach(LongMapConsumer<Object> consumer) {
+
+        }
+    };
+
     public static <V> ImmutableLongMapBuilder<V> builder() {
         return new ImmutableLongOpenMapBuilder<V>();
     }
 
     public static <V> ImmutableLongMapBuilder<V> builder(long emptyKey) {
         return new ImmutableLongOpenMapBuilder<V>(emptyKey);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <V> ImmutableLongMap<V> empty() {
+        return EMPTY;
     }
 
     long emptyKey();

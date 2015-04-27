600fc96218c98182c4d693eb6299f2286c0f4eca
Alexey Korchevskiy <mitallast@gmail.com>
2015-04-27 19:24:44 +0300

    transaction pop request refactor

--- src/main/java/org/mitallast/queue/action/queue/transactional/pop/TransactionPopRequest.java
+++ src/main/java/org/mitallast/queue/action/queue/transactional/pop/TransactionPopRequest.java
@@ -1,20 +1,18 @@
 package org.mitallast.queue.action.queue.transactional.pop;
 
-import org.mitallast.queue.action.ActionRequest;
 import org.mitallast.queue.action.ActionRequestValidationException;
 import org.mitallast.queue.action.ActionType;
+import org.mitallast.queue.action.queue.pop.PopRequest;
 import org.mitallast.queue.common.stream.StreamInput;
 import org.mitallast.queue.common.stream.StreamOutput;
-import org.mitallast.queue.common.strings.Strings;
 
 import java.io.IOException;
 import java.util.UUID;
 
 import static org.mitallast.queue.action.ValidateActions.addValidationError;
 
-public class TransactionPopRequest extends ActionRequest {
+public class TransactionPopRequest extends PopRequest {
 
-    private String queue;
     private UUID transactionUUID;
 
     @Override
@@ -24,22 +22,11 @@ public class TransactionPopRequest extends ActionRequest {
 
     @Override
     public ActionRequestValidationException validate() {
-        ActionRequestValidationException validationException = null;
-        if (Strings.isEmpty(queue)) {
-            validationException = addValidationError("queue is missing", null);
-        }
+        ActionRequestValidationException validationException = super.validate();
         if (transactionUUID == null) {
-            validationException = addValidationError("transactionUUID is missing", null);
+            validationException = addValidationError("transactionUUID is missing", validationException);
         }
         return validationException;
-    }
-
-    public String getQueue() {
-        return queue;
-    }
-
-    public void setQueue(String queue) {
-        this.queue = queue;
     }
 
     public UUID getTransactionUUID() {
@@ -52,13 +39,13 @@ public class TransactionPopRequest extends ActionRequest {
 
     @Override
     public void readFrom(StreamInput stream) throws IOException {
-        queue = stream.readText();
+        super.readFrom(stream);
         transactionUUID = stream.readUUID();
     }
 
     @Override
     public void writeTo(StreamOutput stream) throws IOException {
-        stream.writeText(queue);
+        super.writeTo(stream);
         stream.writeUUID(transactionUUID);
     }
 }

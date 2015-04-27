4ca14e43362aa0a02c706bae3e51841b46d0fd66
Alexey Korchevskiy <mitallast@gmail.com>
2015-04-27 19:28:15 +0300

    transaction delete request refactor

--- src/main/java/org/mitallast/queue/action/queue/transactional/delete/TransactionDeleteRequest.java
+++ src/main/java/org/mitallast/queue/action/queue/transactional/delete/TransactionDeleteRequest.java
@@ -1,22 +1,19 @@
 package org.mitallast.queue.action.queue.transactional.delete;
 
-import org.mitallast.queue.action.ActionRequest;
 import org.mitallast.queue.action.ActionRequestValidationException;
 import org.mitallast.queue.action.ActionType;
+import org.mitallast.queue.action.queue.delete.DeleteRequest;
 import org.mitallast.queue.common.stream.StreamInput;
 import org.mitallast.queue.common.stream.StreamOutput;
-import org.mitallast.queue.common.strings.Strings;
 
 import java.io.IOException;
 import java.util.UUID;
 
 import static org.mitallast.queue.action.ValidateActions.addValidationError;
 
-public class TransactionDeleteRequest extends ActionRequest {
+public class TransactionDeleteRequest extends DeleteRequest {
 
-    private String queue;
     private UUID transactionUUID;
-    private UUID messageUUID;
 
     @Override
     public ActionType actionType() {
@@ -25,25 +22,11 @@ public class TransactionDeleteRequest extends ActionRequest {
 
     @Override
     public ActionRequestValidationException validate() {
-        ActionRequestValidationException validationException = null;
-        if (Strings.isEmpty(queue)) {
-            validationException = addValidationError("queue is missing", null);
-        }
+        ActionRequestValidationException validationException = super.validate();
         if (transactionUUID == null) {
             validationException = addValidationError("transactionUUID is missing", null);
         }
-        if (messageUUID == null) {
-            validationException = addValidationError("messageUUID is missing", null);
-        }
         return validationException;
-    }
-
-    public String getQueue() {
-        return queue;
-    }
-
-    public void setQueue(String queue) {
-        this.queue = queue;
     }
 
     public UUID getTransactionUUID() {
@@ -54,25 +37,15 @@ public class TransactionDeleteRequest extends ActionRequest {
         this.transactionUUID = transactionUUID;
     }
 
-    public UUID getMessageUUID() {
-        return messageUUID;
-    }
-
-    public void setMessageUUID(UUID messageUUID) {
-        this.messageUUID = messageUUID;
-    }
-
     @Override
     public void readFrom(StreamInput stream) throws IOException {
-        queue = stream.readText();
+        super.readFrom(stream);
         transactionUUID = stream.readUUID();
-        messageUUID = stream.readUUID();
     }
 
     @Override
     public void writeTo(StreamOutput stream) throws IOException {
-        stream.writeText(queue);
+        super.writeTo(stream);
         stream.writeUUID(transactionUUID);
-        stream.writeUUID(messageUUID);
     }
 }

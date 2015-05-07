88d54c719fd1e72fae642a3256f9d594fc818946
Alexey Korchevskiy <mitallast@gmail.com>
2015-05-07 15:34:22 +0300

    named executors single thread executor

--- src/main/java/org/mitallast/queue/common/concurrent/NamedExecutors.java
+++ src/main/java/org/mitallast/queue/common/concurrent/NamedExecutors.java
@@ -5,6 +5,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class NamedExecutors {
 
+    public static ExecutorService newSingleThreadPool(String name) {
+        return new ThreadPoolExecutor(1, 1,
+            0L, TimeUnit.MILLISECONDS,
+            new LinkedBlockingQueue<>(),
+            new NamedThreadFactory(name));
+    }
+
     public static ExecutorService newFixedThreadPool(String name, int nThreads) {
         return new ThreadPoolExecutor(
             nThreads,
@@ -17,6 +24,36 @@ public class NamedExecutors {
 
     public static ThreadFactory newThreadFactory(String name) {
         return new NamedThreadFactory(name);
+    }
+
+    public static boolean terminate(ExecutorService service) {
+        if (service != null) {
+            service.shutdown();
+            try {
+                if (service.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS)) {
+                    return true;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            service.shutdownNow();
+        }
+        return false;
+    }
+
+    public static boolean terminate(ExecutorService service, long timeout, TimeUnit timeUnit) {
+        if (service != null) {
+            service.shutdown();
+            try {
+                if (service.awaitTermination(timeout, timeUnit)) {
+                    return true;
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            service.shutdownNow();
+        }
+        return false;
     }
 
     private static class NamedThreadFactory implements ThreadFactory {

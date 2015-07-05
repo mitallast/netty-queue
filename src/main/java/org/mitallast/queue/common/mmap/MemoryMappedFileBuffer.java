e8d17da14389a20b731e71566ab649fdb1589b21
Alexey Korchevskiy <mitallast@gmail.com>
2015-07-05 11:10:29 +0300

    memory mapped file buffer

---
+++ src/main/java/org/mitallast/queue/common/mmap/MemoryMappedFileBuffer.java
@@ -0,0 +1,68 @@
+package org.mitallast.queue.common.mmap;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.io.Closeable;
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class MemoryMappedFileBuffer implements Closeable {
+    private final File file;
+    private final RandomAccessFile randomAccessFile;
+    private final MappedByteBuffer mappedByteBuffer;
+    private final ByteBuf buffer;
+    private volatile boolean closed = false;
+
+    public MemoryMappedFileBuffer(File file, long size) throws IOException {
+        this.file = file;
+        this.randomAccessFile = new RandomAccessFile(file, "rw");
+        FileChannel channel = randomAccessFile.getChannel();
+
+        mappedByteBuffer = channel.map(FileChannel.MapMode.READ_WRITE, 0, size);
+        mappedByteBuffer.load();
+        mappedByteBuffer.order(java.nio.ByteOrder.LITTLE_ENDIAN);
+
+        buffer = Unpooled.wrappedBuffer(mappedByteBuffer);
+    }
+
+    public File file() {
+        return file;
+    }
+
+    public RandomAccessFile randomAccessFile() {
+        return randomAccessFile;
+    }
+
+    public MappedByteBuffer mappedByteBuffer() {
+        return mappedByteBuffer;
+    }
+
+    public ByteBuf buffer() {
+        return buffer;
+    }
+
+    public void flush() throws IOException {
+        synchronized (this) {
+            if (closed) {
+                return;
+            }
+            mappedByteBuffer.force();
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        synchronized (this) {
+            flush();
+            if (closed) {
+                return;
+            }
+            closed = true;
+            MappedByteBufferCleaner.clean(mappedByteBuffer);
+        }
+    }
+}

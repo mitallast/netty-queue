dbf1e7b9b9995bec55d40397eedd104dd28760f9
a.korchevsky <a.korchevsky@corp.mail.ru>
2019-08-02 16:57:24 +0300

    add hex util

--- src/main/java/org/mitallast/queue/common/Hex.java
+++ src/main/java/org/mitallast/queue/common/Hex.java
@@ -1,6 +1,8 @@
 package org.mitallast.queue.common;
 
 public final class Hex {
+    private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+
     public static byte[] parseHexBinary(String s) {
         int len = s.length();
         byte[] data = new byte[len / 2];
@@ -9,5 +11,15 @@ public final class Hex {
                 + Character.digit(s.charAt(i + 1), 16));
         }
         return data;
+    }
+
+    public static String printHexBinary(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 }
